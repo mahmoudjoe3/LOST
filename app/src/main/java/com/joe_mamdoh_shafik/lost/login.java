fa1c6e214fac5e98ff1bd3f8b3d7670b4f7728da
mahmoudjoe3 <mahmoud2017170415@cis.asu.edu.eg>
2019-11-23 20:34:15 +0200

    first time login

--- app/src/main/java/com/joe_mamdoh_shafik/lost/login.java
+++ app/src/main/java/com/joe_mamdoh_shafik/lost/login.java
@@ -3,6 +3,7 @@ package com.joe_mamdoh_shafik.lost;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -14,13 +15,30 @@ public class login extends AppCompatActivity {
 
     private EditText editTextMobile;
     private FirebaseAuth mAuth;
+    SharedPreferences preferences;
+    String firstTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        forFirstTime();
+
+
         editTextMobile = (EditText) findViewById(R.id.phone_txt);
     }
+
+    public void forFirstTime()
+    {
+        preferences=getSharedPreferences("PREFERENCE",MODE_PRIVATE);
+         firstTime=preferences.getString("first_time_install","");
+        if(firstTime.equals("yes")){
+            Intent intent = new Intent(login.this, main_page.class);
+            startActivity(intent);
+        }
+    }
+
+
 
     public void login(View view) {
 
@@ -29,9 +47,16 @@ public class login extends AppCompatActivity {
         if(mobile.isEmpty() || mobile.length() < 10){
             editTextMobile.setError("Enter a valid mobile");
             editTextMobile.requestFocus();
+
             return;
         }
-        Intent intent = new Intent(login.this, Verification.class);
+        //store yes for first time he login
+        if(!firstTime.equals("yes")){
+            SharedPreferences.Editor editor=preferences.edit();
+            editor.putString("first_time_install","yes");
+            editor.apply();
+        }
+        Intent intent = new Intent(login.this, main_page.class);
         intent.putExtra("mobile", mobile);
         startActivity(intent);
     }
